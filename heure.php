590816970b5daa63560a5c5466d9770a69ab1cfb
Ryan94DZ1 <144158756+Ryan94DZ1@users.noreply.github.com>
2023-09-27 13:55:47 +0200

    Add files via upload

---
+++ heure.php
@@ -0,0 +1,8 @@
+        
+<?php
+        $heures = fgets( STDIN ) ;
+        $minutes = fgets( STDIN ) ;
+        $minutesApres = $minutes + 1 ;
+        echo "Dans une minute, il sera " , $heures , ":" $minutesApres , "\n" ;
+
+?>
---
+++ heurePosterieure.php
@@ -0,0 +1,8 @@
+<?php
+echo "heurePosterieure :" ;
+	$heures = 22 ;
+	$minutes = 50 ;
+	$minutesApres = ($minutes + 1) ;
+	echo "Dans une minute, il sera " , $heures , ":" $minutesApres , "\n" ;
+
+?>
